231e4dc2195d0d2af06d40752e1176b4eb55c97d
mwklose <markklose17@gmail.com>
2024-11-05 13:26:22 -0500

    test setup part 2 - examples

---
+++ code/func2.R
@@ -0,0 +1,3 @@
+func2 = function(a, b) {
+    return(a * b)
+}
--- tests/test_test_code.R
+++ tests/test_test_code.R
@@ -1,7 +1,12 @@
 library(testthat)
 
-lapply(list.files("code", pattern = "\\.R$", full.names = TRUE), source)
+filenames = list.files("code", pattern = "\\.R$", full.names = TRUE)
+lapply(filenames, source)
 
 test_that("example function works correctly", {
   expect_equal(test_function(3,5), 8) 
+})
+
+test_that("func2 works correctly", {
+  expect_equal(func2(3,5), 15) 
 })
