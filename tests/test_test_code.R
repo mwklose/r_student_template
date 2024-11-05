ba8170bfbe8052062644cd8d9327dad347bc3fed
mwklose <markklose17@gmail.com>
2024-11-05 15:39:47 -0500

    Incorrect file name

--- tests/test_test_code.R
+++ tests/test_test_code.R
@@ -2,7 +2,7 @@ library(testthat)
 library(here)
 
 source(here::here("code", "func2.R"))
-source(here::here("code", "test_code"))
+source(here::here("code", "test_code.R"))
 
 test_that("example function works correctly", {
   expect_equal(test_function(3,5), 4) 
