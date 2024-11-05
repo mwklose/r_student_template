9cdf2a3faf81a467eaa74267a9296bc834a3789a
mwklose <markklose17@gmail.com>
2024-11-05 13:16:58 -0500

    Test setup - see if takes 10 minutes each time

--- tests/test_test_code.R
+++ tests/test_test_code.R
@@ -1,7 +1,7 @@
 library(testthat)
 
-lapply(list.files("R", pattern = "\\.R$", full.names = TRUE), source)
+lapply(list.files("code", pattern = "\\.R$", full.names = TRUE), source)
 
 test_that("example function works correctly", {
-  expect_equal(test_function(3,5), 8)  # Assuming students implement `example_function`
+  expect_equal(test_function(3,5), 8) 
 })
